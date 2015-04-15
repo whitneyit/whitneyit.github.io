71259dc246ea1bee8c2f7e7bf1c83476af50079e
James Whitney <james@whitney.io>
2015-04-15 16:53:28 +1000

    Ensure that only our specs will be loaded from the `tests` directory

--- tests/karma.main.ts
+++ tests/karma.main.ts
@@ -1,16 +1,18 @@
 /// <reference path="../typings/requirejs/require.d.ts" />
+
 interface Window {
     __karma__ :any;
     requirejs :Require;
     rjsconfig :RequireConfig;
 }
+
 window.rjsconfig.baseUrl = '/base';
 window.rjsconfig.callback = () => {
     window.__karma__.start();
 };
 window.rjsconfig.deps = Object.keys(window.__karma__.files)
     .filter((file) => {
-        return (/\.spec\.js$/i).test(file);
+        return (/^\/base\/tests/).test(file) && (/\.spec\.js$/i).test(file);
     })
     .map((path) => {
         return path.replace(/^\/base\//, '').replace(/\.js$/, '');
